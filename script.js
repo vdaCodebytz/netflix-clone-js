8ec3416c354486efd2fa55c859fc44373e93ac2d
vdaCodebytz <vishnu.3b@gmail.com>
2023-10-05 23:05:04 +0530

    movie api integrated

--- script.js
+++ script.js
@@ -32,3 +32,40 @@ function onPlayerReady(event) {
   event.target.playVideo(); // Auto-play the video when ready
   event.target.mute();
 }
+
+const url = "https://imdb-top-100-movies.p.rapidapi.com/";
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "60b3e8602fmsh6ea0511791b6d5ap1637bcjsn92dec7726b7e",
+    "X-RapidAPI-Host": "imdb-top-100-movies.p.rapidapi.com",
+  },
+};
+
+let cards = document.querySelectorAll(".card");
+
+const getData = async (url, options) => {
+  let response = await fetch(url, options);
+  let result = await response.json();
+
+  let titleMovieNumber = Math.floor(Math.random() * 100 + 1);
+
+  let title = document.querySelector(".hero-content-left h1");
+  let desc = document.querySelector(".hero-content-left p");
+  let heroPoster = document.querySelector(".hero-poster");
+
+  title.innerHTML = result[titleMovieNumber].title;
+  desc.innerHTML = result[titleMovieNumber].description;
+  heroPoster.src = result[titleMovieNumber].image;
+
+  cards.forEach((card) => {
+    let cardImage = document.createElement("img");
+    card.appendChild(cardImage);
+    let movieNumber = Math.floor(Math.random() * 100 + 1);
+    let thumbnailLink = result[movieNumber].image;
+    card.childNodes[0].src = `${thumbnailLink}`;
+  });
+};
+// console.log(cards);
+
+getData(url, options);
